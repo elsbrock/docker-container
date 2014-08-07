58762434b41c9585a9d72deafdceb694365820a2
schinken <schinken@hackerspace-bamberg.de>
2014-08-07 11:47:32 +0200

    Initial mopidy stream commit

---
+++ mopidy-stream/run/mopidy.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+if [ -n "$MOPIDY_PASSWORD" ]; then
+    sed -i "s/password = demo/password = $MOPIDY_PASSWORD/g" /etc/mopidy/mopidy.conf
+fi
+
+if [ -n "$MOPIDY_USERNAME" ]; then
+    sed -i "s/username = demo/username = $MOPIDY_USERNAME/g" /etc/mopidy/mopidy.conf
+fi
+
+exec mopidy --config /etc/mopidy/mopidy.conf
